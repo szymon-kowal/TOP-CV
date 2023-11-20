1edd3ad7ee264e4d9b8151ed5448bfc7e16038ab
szymek <00szymon.kowal@gmail.com>
2023-11-20 09:59:36 +0100

    Inital commit

---
+++ src/client/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/styles/styles.css";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
